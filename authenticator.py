c67ba474f383fcdf344f2dd2cebe6d160c5ee762
Timo Riedinger <mail@timoriedinger.de>
2022-04-27 21:19:39 +0200

    Done

--- authenticator.py
+++ authenticator.py
@@ -7,11 +7,10 @@ def auth(try_user, try_password):
     shadow = Reader("shadow")
     users = shadow.getUsernames()
     passwords = shadow.getEncryptedUserPassword()
-    password_hash = hashlib.sha512(str(try_password).encode("utf-8")).hexdigest()
 
     for counter in range(len(users)):
         if users[counter].lower() == try_user.lower():
-            if passwords[counter] == password_hash:
+            if passwords[counter] == str(try_password):
                 return True
 
     return False
--- py_ssh_server.py
+++ py_ssh_server.py
@@ -1,24 +1,24 @@
 import socket
 import subprocess
 import sys
-import pyDH
 import rsa
+import rsa.randnum
+from Crypto.Cipher import AES
 
 import authenticator
 
-diffieHellman = pyDH.DiffieHellman()
-
 class Py_ssh_server:
 
-    def __init__(self, host="", port=22):
+    def __init__(self, host="", port=22, hostname="localhost"):
         self.generateKeys()
         self.HOST = host
+        self.HOSTNAME = hostname
         self.PORT = port
         self.user = ""
         self.password = ""
+        (self.publicKey, self.privateKey, self.sharedKey) = self.loadKeys()
         self.auth = False
-        (self.publicKey, self.privateKey) = self.loadKeys()
-        self.sharedKey = None
+        self.recievedPublicKey = None
 
     def start(self):
 
@@ -31,27 +31,13 @@ class Py_ssh_server:
 
             with conn:
 
-                self.sendPublicKey(conn)
-
-                recivevedpub = self.recievePublicKey(s)
-                recivevedpub = str(recivevedpub).split("(")[1].split(",")
-                recivevedpub[1] = recivevedpub[1].split(")")[0].strip()
-
-                keyN = int(recivevedpub[0])
-                keyE = int(recivevedpub[1])
-                pub = rsa.PublicKey(keyN, keyE)
-
-                self.sharedKey = diffieHellman.gen_shared_key(
-                    pub
-                )
-
-                self.send(conn, "User:")
+                self.publicKeyTransaction(conn)
+                self.sendSharedKey(conn)
 
                 while True:
 
                     if not self.auth:
-                        data = self.recieve(conn)
-                        data = self.authentication(conn, data)
+                        data = self.authentication(conn)
                     else:
                         data = self.commands(conn)
 
@@ -61,16 +47,30 @@ class Py_ssh_server:
 
                     print(data)
 
-    def authentication(self, conn, data):
+    def publicKeyTransaction(self, conn):
+
+        self.sendPublicKey(conn)
+
+        recivedPublicKey = self.recievePublicKey(conn)
+        recivedPublicKey = str(recivedPublicKey).split("(")[1].split(",")
+        recivedPublicKey[1] = recivedPublicKey[1].split(")")[0].strip()
+
+        keyN = int(recivedPublicKey[0])
+        keyE = int(recivedPublicKey[1])
+
+        self.recievedPublicKey = rsa.PublicKey(keyN, keyE)
+
+
+    def authentication(self, conn):
 
         if self.user == "":
+            self.send(conn, "User:")
+            data = self.recieve(conn)
             self.user = data
-            if self.user != "":
-                self.send(conn, "Password:")
-            else:
-                print("Username not recived!")
 
         elif self.password == "":
+            self.send(conn, "Password:")
+            data = self.recieve(conn)
             self.password = data
             self.auth = authenticator.auth(self.user, self.password)
 
@@ -78,12 +78,14 @@ class Py_ssh_server:
                 self.send(conn, "auth error")
                 conn.close()
                 sys.exit("Authentifizierung fehlgeschlagen")
+        else:
+            data = "Error"
 
         return data
 
     def commands(self, conn):
 
-        self.send(conn, "{}@Server:".format(self.user))
+        self.send(conn, "{}@{}:".format(self.user, self.HOSTNAME))
         command = self.recieve(conn).split(" ")
 
         if command[0].lower() == "exit":
@@ -99,21 +101,39 @@ class Py_ssh_server:
 
         return output
 
-    def sendPublicKey(self, conn):
-        conn.sendall(bytes("{}".format(self.publicKey), "utf-8"))
-
-    def send(self, conn, message):
-        message = rsa.encrypt(message.encode('ascii'), self.publicKey)
-        conn.sendall(bytes("{}".format(message), "utf-8"))
 
     def recievePublicKey(self, sock):
         data = sock.recv(1024)
         return data
 
+    def sendPublicKey(self, conn):
+        conn.sendall(bytes("{}".format(self.publicKey), "utf-8"))
+
+    def testVerschluesselung(self):
+        print("SharedKey: {}".format(self.sharedKey))
+        message = rsa.encrypt(self.sharedKey, self.publicKey)
+        print("Encrypted SharedKey: {}".format(message))
+        message = rsa.decrypt(message, self.privateKey)
+        print("Entschlüsselter SharedKey: {}".format(message))
+        sys.exit("Test Ver- und entschlüsselung durchgeführt")
+
+    def sendSharedKey(self, conn):
+        message = rsa.encrypt(self.sharedKey, self.recievedPublicKey)
+        #conn.sendall(bytes("{}".format(message), "utf-8"))
+        conn.sendall(bytes("{}".format(self.sharedKey), "utf-8"))
+
+
+    def send(self, conn, message):
+        sender = AES.new("self.sharedKey12", AES.MODE_ECB)
+        #message = sender.encrypt(message*16)
+        conn.sendall(bytes("{}".format(message), "utf-8"))
+
     def recieve(self, conn):
-        data = conn.recv(1024)
-        data = rsa.decrypt(data, self.sharedKey)
+        data = conn.recv(1024).decode("utf-8")
+        reciever = AES.new("self.sharedKey12", AES.MODE_ECB)
+        #data = reciever.decrypt(data)
         return str(data)
+
 
     def generateKeys(self):
         (publicKey, privateKey) = rsa.newkeys(1024)
@@ -121,10 +141,14 @@ class Py_ssh_server:
             p.write(publicKey.save_pkcs1('PEM'))
         with open('keys/privateKey.pem', 'wb') as p:
             p.write(privateKey.save_pkcs1('PEM'))
+        with open('keys/sharedKey', 'wb') as p:
+            p.write(rsa.randnum.read_random_bits(128))
 
     def loadKeys(self):
         with open('keys/publicKey.pem', 'rb') as p:
             publicKey = rsa.PublicKey.load_pkcs1(p.read())
         with open('keys/privateKey.pem', 'rb') as p:
             privateKey = rsa.PrivateKey.load_pkcs1(p.read())
-        return publicKey, privateKey
+        with open('keys/sharedKey', 'rb') as s:
+            sharedKey = s.read()
+        return publicKey, privateKey, sharedKey
